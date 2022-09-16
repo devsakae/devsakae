315daff4a1db67d8e673a7535887b3e7c3a8ddec
Rodrigo Sakae <devsakae@gmail.com>
2022-09-16 18:32:40 -0300

    necessário refatoração

--- projetos/restriboaurante/src/App.js
+++ projetos/restriboaurante/src/App.js
@@ -18,18 +18,6 @@ class App extends Component {
     }))
   }
 
-  handleChange = ({ target }) => {
-    const { value } = target;
-    this.setState({
-      total: Number(value) * 60,
-      input: Number(value) * 60
-    },() => {
-      setTimeout(() => {
-        target.value = ''
-      },10000)
-    })
-  }
-
   timer = () => {
     this.setState({ click: true })
     const { total } = this.state;
@@ -85,14 +73,19 @@ class App extends Component {
 
   render() {
     const { total, click } = this.state;
-    const minutes = Math.floor(total / 60) //pois se nao daria um numero quebrado por exemplo 4.98888 nos minutos , os segundo iariam permanecer normais
-    const seconds = total % 60
+    const minutes = Math.floor(total / 60);
+    const seconds = total % 60;
+    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
     
     return (
-      <div>
+      <main>
         <div>
-          <button type="button" onClick={ () => this.addTime(0.5) }>
-            Bolacha (30 segundos)
+          Menu
+        </div>
+        <div>
+          <button type="button" onClick={ () => this.addTime(0.05) }>
+            Teste (2 segundos)
           </button>
           <button type="button" onClick={ () => this.addTime(0.75) }>
             Amendoim (45 segundos)
@@ -117,18 +110,25 @@ class App extends Component {
             Chocolate quente (+5 minutos)
           </button>
         </div>
-        <h1>Timer</h1>
-          <input
-            name="total"
-            type='number'
-            placeholder="Minutos"
-            onChange={ this.handleChange }
-          />
-        <div className="timer">
-          <span>{minutes.toString().padStart(2, "0")}</span>
-          <span>:</span>
-          <span>{seconds.toString().padStart(2, "0")}</span>
-        </div>
+        { total === 0 ? click && <h4>Terminou o intervalo!</h4> :
+          (<div className="countdown-wrapper">
+              { minutes && (
+                  <div className="countdown-item">
+                      <SVGCircle radius={ minutesRadius } />
+                      { minutes.toString().padStart(2, "0") }
+                      <span>minutos</span>
+                  </div>
+              )}
+              { seconds && (
+                  <div className="countdown-item">
+                      <SVGCircle radius={ secondsRadius } />
+                      { seconds.toString().padStart(2, "0") }
+                      <span>segundos</span>
+                  </div>
+              )}
+          </div>)        
+        }
+
         <div className="App">
           <div className="functions">
 
@@ -144,12 +144,60 @@ class App extends Component {
           </div>
 
           </div>
-          {
-            total === 0 && click && <h4>Obrigado  :)</h4>
-          }
-      </div>
+      </main>
     )
   }
+}
+
+const SVGCircle = ({ radius }) => (
+  <svg className="countdown-svg">
+      <path
+        fill="none"
+        stroke="#333"
+        strokeWidth="5"
+        d={describeArc(50, 50, 48, 0, radius)}
+      />
+  </svg>
+);
+
+// From StackOverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+  return {
+      x: centerX + radius * Math.cos(angleInRadians),
+      y: centerY + radius * Math.sin(angleInRadians)
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+  var d = [
+      'M',
+      start.x,
+      start.y,
+      'A',
+      radius,
+      radius,
+      0,
+      largeArcFlag,
+      0,
+      end.x,
+      end.y
+  ].join(' ');
+
+  return d;
+}
+
+// From StackOverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+  return (
+      ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  );
 }
 
 export default App;
